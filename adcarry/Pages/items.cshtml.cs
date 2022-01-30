2abcf11ee174ec3e56436f11feb79ee83994d619
Zajicek-Adam <adam.zajicek.020@pslib.cz>
2022-01-30 23:57:33 +0100

    IDK

---
+++ adcarry/Pages/items.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace adcarry.Pages
+{
+    public class itemsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ adcarry/Pages/playstyle.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace adcarry.Pages
+{
+    public class playstyleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
